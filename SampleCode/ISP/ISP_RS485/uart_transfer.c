922fb3c18c06995921da21a0b9f654c1e5247e4c
YTLu <YTLu@nuvoton.com>
2023-04-11 09:48:34 +0800

    Modify sample code for KEIL Compiler 6.
    Change-Id: I7fca95663364446b409555cffd03a6a466839f32

--- SampleCode/ISP/ISP_RS485/uart_transfer.c
+++ SampleCode/ISP/ISP_RS485/uart_transfer.c
@@ -11,7 +11,7 @@
 #include <string.h>
 #include "targetdev.h"
 
-__align(4) uint8_t  uart_rcvbuf[MAX_PKT_SIZE] = {0};
+__attribute__((aligned(4))) uint8_t  uart_rcvbuf[MAX_PKT_SIZE] = {0};
 
 uint8_t volatile bUartDataReady = 0;
 uint8_t volatile bufhead = 0;
@@ -44,7 +44,7 @@ void UART_N_IRQHandler(void)
     }
 }
 
-extern __align(4) uint8_t response_buff[64];
+extern __attribute__((aligned(4))) uint8_t response_buff[64];
 void PutString(void)
 {
     uint32_t i;
